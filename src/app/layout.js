93f3682f37174b5d9a038a8d5789fbc267f5dc40
jamalmatic <jamalmatic@gmail.com>
2023-07-23 11:30:36 +0100

    add roboto font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,9 @@
-
+import { Roboto } from 'next/font/google'
+ 
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: 'Turbo car rental management',
@@ -8,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={roboto.className}>
+        {children}
+      </body>
     </html>
   )
 }
